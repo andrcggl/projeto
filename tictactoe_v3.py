e744eaa61c41fccc0cf2d885dd40a34a1dc9e7b0
andrcggl <andrecoggiola@gmail.com>
2021-09-16 10:27:03 -0300

    nova versão

--- tictactoe_v3.py
+++ tictactoe_v3.py
@@ -4,11 +4,45 @@ board = {'a1': ' ', 'b1': ' ', 'c1': ' ',
          'a2': ' ', 'b2': ' ', 'c2': ' ',
          'a3': ' ', 'b3': ' ', 'c3': ' '}
 
-moves = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']
+def main():
+    print('\n**Welcome to Python Tic-Tac-Toe!**\n')
+    print('Play against:\n1. CPU\n2. Human')
+    choice = input('Choose (1 or 2):')
+    if choice == '1':
+        tictac(human_player, cpu_easy)
+    elif choice == '2':
+        tictac(human_player, human_player)
+    else:
+        print('1 or 2, please.')
+        main()
 
-turn = 'X'
+def tictac(player1, player2):  # player1 and 2 are functions
+    print('\nGame start\n')
+    print_board()
+    i = 1
+    current_player = player1
+    turn = 'X'
+    while i <= len(board.keys()):
+        print('\n'+ turn + '\'s turn.')
+        current_player(turn)
+        print_board()
+        if is_game_over(turn):
+            print('\nGame Over.\n')
+            print('*** ' + turn + ' won! ***')
+            play_again(player1, player2)
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'
+        if current_player == player1:
+            current_player = player2
+        else:
+            current_player = player1
+        i += 1
+    print('\nGame Over.\nIt is a tie.')
+    play_again(player1, player2)
 
-def print_board(board):
+def print_board():
     print('  a b c')
     print('1 ' + board['a1'] + '|' + board['b1'] + '|' + board['c1'])
     print('  -+-+-')
@@ -17,113 +51,59 @@ def print_board(board):
     print('3 ' + board['a3'] + '|' + board['b3'] + '|' + board['c3'])
 
 def legalMove(move):
-    if move in moves:
+    if move in board.keys():
         if board[move] == ' ':
             return True
     else:
         return False
 
-def play_again():
+def play_again(player1, player2):
     print('\nWould you like to play again?')
     answer = input('Y or N: ')
     if answer == 'y' or answer == 'Y':
-        for i in moves:
+        for i in board.keys():
             board[i] = ' '
-        tictac(player1, player2, turn)
+        tictac(player1, player2)
     elif answer == 'n' or answer == 'N':
         exit()
     else:
         print('Y or N, please.')
         play_again()
 
-def game_over(board, turn):
+def is_game_over(turn):
     if board['a1'] == board['a2'] == board['a3'] != ' ':  # a column
-        print('\nGame Over.\n')
-        print('*** ' + turn + ' won! ***')
-        play_again()
+        return True
     elif board['b1'] == board['b2'] == board['b3'] != ' ':  # b column
-        print('\nGame Over.\n')
-        print('*** ' + turn + ' won! ***')
-        play_again()
+        return True
     elif board['c1'] == board['c2'] == board['c3'] != ' ':  # c column 
-        print('\nGame Over.\n')
-        print('*** ' + turn + ' won! ***')
-        play_again()
+        return True
     elif board['a1'] == board['b1'] == board['c1'] != ' ':  # 1 row
-        print('\nGame Over.\n')
-        print('*** ' + turn + ' won! ***')
-        play_again()
+        return True
     elif board['a2'] == board['b2'] == board['c2'] != ' ':  # 2 row
-        print('\nGame Over.\n')
-        print('*** ' + turn + ' won! ***')
-        play_again()
+        return True
     elif board['a3'] == board['b3'] == board['c3'] != ' ':  # 3 row
-        print('\nGame Over.\n')
-        print('*** ' + turn + ' won! ***')
-        play_again()
+        return True
     elif board['a1'] == board['b2'] == board['c3'] != ' ':  # diagonal
-        print('\nGame Over.\n')
-        print('*** ' + turn + ' won! ***')
-        play_again()
+        return True
     elif board['a3'] == board['b2'] == board['c1'] != ' ':  # diagonal
-        print('\nGame Over.\n')
-        print('*** ' + turn + ' won! ***')
-        play_again()
+        return True
 
-def human_player(board, turn):
+def human_player(turn):
     move = input('\nYour move: ')
     if legalMove(move):
         board[move] = turn
     else:
         print('\nIlegal move. Play again.')
-        human_player(board, turn)
+        human_player(turn)
     return board
 
-def cpu_easy(board, turn):
-    for move in random.sample(moves, len(moves)):
+def cpu_easy(turn):
+    for move in random.sample(board.keys(), len(board.keys())):
         if legalMove(move):
             board[move] = turn
             return board
 
-player1 = human_player
 
-player2 = cpu_easy
-
-def tictac(player1, player2, turn):  # player1 and 2 are functions
-    print('\nGame start\n')
-    print_board(board)
-    i = 1
-    player = player1
-    while i < 9:
-        print('\n'+ turn + '\'s turn.')
-        print_board(player(board, turn))
-        game_over(board, turn)
-        if turn == 'X':
-            turn = 'O'
-        else:
-            turn = 'X'
-        if player == player1:
-            player = player2
-        else:
-            player = player1
-        i += 1
-    print('\nGame Over.\nIt is a tie.')
-    exit()
-        
-def main():
-    print('\n**Welcome to Python Tic-Tac-Toe!**\n')
-    print('Play against:\n1. CPU\n2. Human')
-    choice = input('Choose (1 or 2):')
-    if choice == '1':
-        tictac(player1, cpu_easy, turn)
-    elif choice == '2':
-        player2 = human_player
-        tictac(player1, player2, turn)
-    else:
-        print('1 or 2, please.')
-        main()
-
-
-#tictac(player1, player2, turn)
+#tictac(cpu_easy, cpu_easy, turn)
 
 main()
